565b2698f048c5fc5a13ecf11a08efb740e4d83a
entropia.studio <info@entropia.studio>
2018-10-29 12:05:52 +0100

    Add Not Found, Top Menu, .env and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PollsComponent } from './components/polls/polls.component';
+import { NotFoundComponent} from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: PollsComponent },
+  { path: 'polls' , component: PollsComponent},
+  { path: 'polls/:userId' , component: PollsComponent},
+  { path: '**' , component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopmenuComponent } from './components/topmenu/topmenu.component';
+import { PollsComponent } from './components/polls/polls.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopmenuComponent,
+    PollsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
